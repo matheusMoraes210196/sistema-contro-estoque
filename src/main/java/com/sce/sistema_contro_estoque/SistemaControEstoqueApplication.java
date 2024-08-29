2a690e687dea9b645c9dc53e7d28f8ff25148c96
Matheus Ramon De Moura Moraes <matheusmoraes2101@yahoo.com.br>
2024-08-29 17:50:42 -0300

    criando base do projeto

---
+++ src/main/java/com/sce/sistema_contro_estoque/SistemaControEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.sce.sistema_contro_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaControEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaControEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sce/sistema_contro_estoque/SistemaControEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sce.sistema_contro_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaControEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
