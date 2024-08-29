4e6a1d793720c1d0138de3ccc25838f1a236c7bc
Matheus Ramon De Moura Moraes <matheusmoraes2101@yahoo.com.br>
2024-08-29 18:27:49 -0300

    Criando primeiras classes do projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,19 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.mapstruct:mapstruct:1.6.0")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports{
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.3")
+	}
 }
 
 tasks.withType<Test> {
